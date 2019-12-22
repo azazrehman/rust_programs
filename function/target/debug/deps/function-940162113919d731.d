463b28357aab24f793360e55813a4516dccd80e8
Azaz ur rehman <azaz.bscs3173@iiu.edu.pk>
2019-12-21 22:31:23 -0800

    Add sample usage of function

---
+++ function/target/debug/deps/function-940162113919d731.d
@@ -0,0 +1,5 @@
+C:\IOT\Programs\function\target\debug\deps\function-940162113919d731.exe: src\main.rs
+
+C:\IOT\Programs\function\target\debug\deps\function-940162113919d731.d: src\main.rs
+
+src\main.rs:
